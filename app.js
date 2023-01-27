09381f48700cf6beb4c6b0e1accd0752263f6e7e
BrianFiset <fisetbrian@gmail.com>
2023-01-27 16:19:00 -0500

    adding classes

--- app.js
+++ app.js
@@ -21,11 +21,13 @@ window.addEventListener('click', (e)=> {
 formContainer.addEventListener('submit', (e)=> e.preventDefault());
 
 
-function Book(title1,author1,pages1,read1) {
-    this.title = title1;
-    this.author = author1;
-    this.pages = pages1;
-    this.read = read1;
+class Book {
+    constructor(title1,author1,pages1,read1){
+        this.title = title1;
+        this.author = author1;
+        this.pages = pages1;
+        this.read = read1;
+    }
 };
 
 function addBookToLibrary() {
